516fafdeab16d72593cfb4d976f1d3069fdd2a02
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-01 15:05:57 +0300

    task 20

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,9 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: List[str],
-                 separator: str) -> str:
+                 separator: str):
+    """Function to replace PI with obsufucated fields
+    """
     return re.sub(rf"({'|'.join(fields)})=([^;{separator}]*)", lambda m:
                   f"{m.group(1)}={redaction}", message)
 
@@ -19,11 +21,13 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields: List[str]):
         """Initialise the class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        """Obsufucate the PI fileds from th e logs"""
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg,
+                                  self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
