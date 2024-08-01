141ded7b9584962768565d8f41b24640d7c12d21
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-01 14:42:43 +0300

    task 1

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to filter and obsfucate PI from logs"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: List[str],
+                 separator: str):
+    """Function to replace PI with obsufucated fields
+    """
+    for field in fields:
+        pattern = re.compile(f"({field}=)([^;{separator}]*)")
+        # Substitute the PII data with the redaction string
+        message = pattern.sub(rf"\1{redaction}", message)
+    return message
