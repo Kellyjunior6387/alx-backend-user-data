c696676c4b818af5343dbefe5645f2ce10b4f5cf
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-01 15:05:04 +0300

    task 20

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,6 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: List[str],
                  separator: str) -> str:
-    """Function to replace PI with obsufucated fields
-    """
     return re.sub(rf"({'|'.join(fields)})=([^;{separator}]*)", lambda m:
                   f"{m.group(1)}={redaction}", message)
 
