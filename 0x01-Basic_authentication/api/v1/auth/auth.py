5e1ef9c6bfa66b5ea4b666ea99e0a115bddfc3c5
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-06 20:33:17 +0300

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,13 @@ class Auth:
     """Class to implement authentication
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        return False
+        if path and excluded_paths:
+            path = path.rstrip('/') + '/'
+            for excluded_path in excluded_paths:
+                excluded_path = excluded_path.rstrip('/') + '/'
+                if path == excluded_path:
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         return None
