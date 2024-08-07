710184844bebe1efe005a8157c5affd3b8be2c3d
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-07 17:10:17 +0300

    imports

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module to manage the API authentication"""
-#from flask import request
+from flask import request
 from typing import List, TypeVar
 
 
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,7 @@
 """
 Module to implement Basic authentication Module to implement Basic authentication 
 """
-from auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
