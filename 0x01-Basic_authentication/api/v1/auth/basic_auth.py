8fcd2e8bc3fe7b4022e87eb3e4e9089279e1ba94
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-07 08:51:37 +0300

    debbuging

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 """
-Module to implement Basic authentication
+Module to implement Basic authentication Module to implement Basic authentication 
 """
 from auth import Auth
-import re
 
 
 class BasicAuth(Auth):
-    """Basic authentication class.
     """
-    def extract_base64_authorization_header(
-            self,
-            authorization_header: str) -> str:
-        """Extracts the Base64 part of the Authorization header
-        for a Basic Authentication.
+    Inherits from Auth and encode and decode headers.
+    """
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
         """
-        if type(authorization_header) == str:
-            pattern = r'Basic (?P<token>.+)'
-            field_match = re.fullmatch(pattern, authorization_header.strip())
-            if field_match is not None:
-                return field_match.group('token')
+        Returns the Base64 part of the Authorization header
+        for a Basic Authentication
+        """
+        if authorization_header and isinstance(authorization_header, str):
+            if authorization_header.startswith('Basic '):
+                return authorization_header.strip('Basic ')
         return None
