1866446483c50d24b0acee846c35f41fe47f2275
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-07 23:00:58 +0300

    task 1

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Authentication module for the API.
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """Authentication class.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Method to check if a path requires authentication
+        """
+        if path and excluded_paths:
+            path = path.rstrip('/') + '/'
+            for excluded_path in excluded_paths:
+                excluded_path = excluded_path.rstrip('/') + '/'
+                if path == excluded_path:
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Gets the authorization header field from the request.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Gets the current user from the request.
+        """
+        return None
