d3aec7618d79904a90d340a7019bfbed35980009
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-07 23:11:05 +0300

    task 2

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module to implement the Session authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Class to implement session authenticaation
+    """
