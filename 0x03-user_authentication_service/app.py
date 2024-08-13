5e697c728b6779ca99d5cdc389fb8c8838960d47
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-13 22:53:53 +0300

    root route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,7 +8,7 @@ AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> None:
+def index() -> str:
     """Start the app"""
     return jsonify({"message": "Bienvenue"})
 
