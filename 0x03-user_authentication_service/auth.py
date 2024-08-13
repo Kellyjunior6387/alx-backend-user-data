99b7d11ed897d310bf591b8ebfa0fdde3f910d62
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-13 15:50:37 +0300

    pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from user import User
 from uuid import uuid4
 from sqlalchemy.orm.exc import NoResultFound
 
+
 def _hash_password(password: str) -> bytes:
     """Method to encrypt passowrd using bcrypt"""
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
