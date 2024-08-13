69aa4aec7f05f803a428994065b843e54dd27291
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-14 00:41:09 +0300

    advanced task 1

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,24 +1,107 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-from db import DB
-from user import User
-
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+"""Module for integration tests"""
+import requests
 
 
-my_db = DB()
+def register_user(email: str, password: str) -> None:
+    """
+    An integration test to test user registration
+    """
+    response = requests.post('http://127.0.0.1:5000/users',
+                             data={'email': email,
+                                   'password': password})
+    if response.status_code == 200:
+        assert(response.json() == {"email": email, "message": "user created"})
+    else:
+        assert(response.status_code == 400)
+        assert(response.json() == {"message": "email already registered"})
 
-email = 'test@test.com'
-hashed_password = "hashedPwd"
 
-user = my_db.add_user(email, hashed_password)
-print(user.id)
+def log_in_wrong_password(email: str, password: str) -> None:
+    """
+    A test for a wrong password login
+    """
+    response = requests.post('http://127.0.0.1:5000/sessions',
+                             data={'email': email,
+                                   'password': password})
+    assert(response.status_code == 401)
 
-try:
-    my_db.update_user(user.id, hashed_password='NewPwd')
-    print("Password updated")
-except ValueError:
-    print("Error")
+
+def profile_unlogged() -> None:
+    """A test for a unlogged profile
+    """
+    response = requests.get('http://127.0.0.1:5000/profile')
+    assert(response.status_code == 403)
+
+
+def log_in(email: str, password: str) -> str:
+    """A test for a correct log in"""
+    res = requests.post('http://127.0.0.1:5000/sessions',
+                        data={'email': email,
+                              'password': password})
+    assert(res.status_code == 200)
+    assert(res.json() == {"email": email, "message": "logged in"})
+    return res.cookies['session_id']
+
+
+def profile_logged(session_id: str) -> None:
+    """A test for a logged in profile
+    """
+    cookie = {'session_id': session_id}
+    response = requests.get('http://127.0.0.1:5000/profile',
+                            cookies=cookie)
+    assert(response.status_code == 200)
+
+
+def log_out(session_id: str) -> None:
+    """ A logout test
+    """
+    cookie = {'session_id': session_id}
+    response = requests.delete('http://127.0.0.1:5000/sessions',
+                               cookies=cookie)
+    if response.status_code == 302:
+        assert(response.url == 'http://127.0.0.1:5000/')
+    else:
+        assert(response.status_code == 200)
+
+
+def reset_password_token(email: str) -> str:
+    """A test for the reset password token
+    """
+    response = requests.post('http://127.0.0.1:5000/reset_password',
+                             data={'email': email})
+    if response.status_code == 200:
+        return response.json()['reset_token']
+    else:
+        assert(response.status_code == 403)
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """A test for updating a forgotten password
+    """
+    res = requests.put('http://127.0.0.1:5000/reset_password',
+                       data={'email': email,
+                             'reset_token': reset_token,
+                             'new_password': new_password})
+    if res.status_code == 200:
+        assert(res.json() == {"email": email, "message": "Password updated"})
+    else:
+        assert(res.status_code == 403)
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
