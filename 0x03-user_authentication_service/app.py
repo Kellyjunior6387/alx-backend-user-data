eb1286649c20de7230009a0ac13151241360f8d8
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-13 11:57:34 +0300

    task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env  python3
 """Module to start a Flask server"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -24,6 +24,23 @@ def users():
             return jsonify({"email": email, "message": "user created"})
         except ValueError:
             return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Endpoint to implement the login function"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email and password:
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            user = AUTH._db.find_user_by(email=email)
+            user.session_id = session_id
+            response = make_response(jsonify({"email": email,
+                                              "message": "logged in"}))
+            response.set_cookie('session_id', session_id)
+            return response
+    abort(401)
 
 
 if __name__ == "__main__":
