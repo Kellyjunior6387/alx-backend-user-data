a61a837ac2ebd25f0d06ca35bc8eebf204418a78
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-13 22:50:53 +0300

    register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,9 +29,9 @@ class Auth:
         if email and password:
             try:
                 self._db.find_user_by(email=email)
-                raise ValueError(f'User {email} already exists')
             except NoResultFound:
                 return self._db.add_user(email, _hash_password(password))
+            raise ValueError(f'User {email} already exists')
 
     def valid_login(self, email: str, password: str) -> bool:
         """Method to validate a user's password"""
